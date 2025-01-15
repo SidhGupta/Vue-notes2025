37c56f4989e2329707daa2216be1e450079ff92b
mr. whalen <michael.whalen@sitechhs.com>
2025-01-15 07:38:28 -0500

    sdf

---
+++ app2/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <h2>{{ user }}</h2>
+    <input type="text" v-model="user" />
+    <button @click="increment">Click Me</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+const count = ref(7)
+const user = ref('')
+function increment() {
+  count.value++
+}
+</script>
+
+<style scoped></style>
