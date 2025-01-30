f5fb23977af6cbceab13713b336ae300300521b4
mr. whalen <michael.whalen@sitechhs.com>
2025-01-30 11:42:40 -0500

    dfghdfg

---
+++ appT/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <h1 class="bg-red-500">dfklhjgbsikl;pdfbhjk</h1>
+  </main>
+</template>
--- tailwind/src/views/HomeView.vue
+++ tailwind/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
   <div class="card bg-base-100 w-96 shadow-xl">
     <figure>
       <img
